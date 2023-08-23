54263beac8a3b2523cb69175a087a8e2cead8bfe
Alyce Barreto <barretomalyce@gmail.com>
2023-08-23 20:58:57 -0300

    adiciona código da questao 3

---
+++ atvd/questao3.java
@@ -0,0 +1,7 @@
+package atvd;
+
+public class Questao3 {
+	public static void main(String[] args) {
+		System.out.println("Terminei a primeira aula com um programa Java!");
+	}
+}
--- atvd/questao6.java
+++ atvd/questao6.java
@@ -1,6 +1,6 @@
 package atvd;
 
-public class Main {
+public class Qeustao6 {
 	public static void main(String[] args) {
 		System.out.println("Bruno Henrique e Maria Alyce");
 		System.out.println("Ambos torcem para o Flamengo");
